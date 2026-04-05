c220ca3e15ddc5530ad43c2728f935e467de7d62
kapiw0n <flatline11@mail.ru>
2026-04-05 14:00:06 +0500

    module4-task1

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -2,37 +2,82 @@ import EditPointView from '../view/edit-point-view.js';
 import EventListView from '../view/event-list-view.js';
 import PointView from '../view/point-view.js';
 import SortView from '../view/sort-view.js';
-import { render, RenderPosition } from '../render.js';
+import { render, replace } from '../framework/render.js';
 
 export default class BoardPresenter {
-  sortComponent = new SortView();
-  eventListComponent = new EventListView();
+  #boardContainer = null;
+  #pointsModel = null;
+
+  #sortComponent = new SortView();
+  #eventListComponent = new EventListView();
+
+  #boardPoints = [];
+  #boardDestinations = [];
+  #boardOffers = [];
 
   constructor({ boardContainer, pointsModel }) {
-    this.boardContainer = boardContainer;
-    this.pointsModel = pointsModel;
+    this.#boardContainer = boardContainer;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
-    this.boardPoints = [...this.pointsModel.points];
-    this.boardDestinations = [...this.pointsModel.destinations];
-    this.boardOffers = [...this.pointsModel.offers];
+    this.#boardPoints = [...this.#pointsModel.points];
+    this.#boardDestinations = [...this.#pointsModel.destinations];
+    this.#boardOffers = [...this.#pointsModel.offers];
 
-    render(this.sortComponent, this.boardContainer);
-    render(this.eventListComponent, this.boardContainer);
+    this.#renderBoard();
+  }
 
-    render(new EditPointView({
-      point: this.boardPoints[0],
-      pointDestinations: this.boardDestinations,
-      pointOffers: this.boardOffers
-    }), this.eventListComponent.getElement(), RenderPosition.AFTERBEGIN);
+  #renderPoint(point) {
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    };
 
-    for (let i = 1; i < this.boardPoints.length; i++) {
-      render(new PointView({
-        point: this.boardPoints[i],
-        pointDestinations: this.boardDestinations,
-        pointOffers: this.boardOffers
-      }), this.eventListComponent.getElement());
+    const pointComponent = new PointView({
+      point,
+      pointDestinations: this.#boardDestinations,
+      pointOffers: this.#boardOffers,
+      onEditClick: () => {
+        replacePointToForm();
+        document.addEventListener('keydown', escKeyDownHandler);
+      }
+    });
+
+    const pointEditComponent = new EditPointView({
+      point,
+      pointDestinations: this.#boardDestinations,
+      pointOffers: this.#boardOffers,
+      onFormSubmit: () => {
+        replaceFormToPoint();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      },
+      onFormClick: () => {
+        replaceFormToPoint();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    });
+
+    function replacePointToForm() {
+      replace(pointEditComponent, pointComponent);
+    }
+
+    function replaceFormToPoint() {
+      replace(pointComponent, pointEditComponent);
+    }
+
+    render(pointComponent, this.#eventListComponent.element);
+  }
+
+  #renderBoard() {
+    render(this.#sortComponent, this.#boardContainer);
+    render(this.#eventListComponent, this.#boardContainer);
+
+    for (let i = 0; i < this.#boardPoints.length; i++) {
+      this.#renderPoint(this.#boardPoints[i]);
     }
   }
 }
